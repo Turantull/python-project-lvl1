3e21b08f571ab8af394cd24910c5019e607a73d5
turantul <python_p@ukr.net>
2020-06-18 20:34:39 +0300

    game even

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,9 +3,66 @@
 """Acquaintance with the user."""
 
 from prompt import string
+from random import randint
+
+
+def welcome():
+    """Welcome!."""
+    print('Welcome to the Brain Games!')
+
+
+def rules_of_game_even():
+    """Rules of the game even."""
+    print('Answer "yes" if number even othewise answer "no".', end = '\n\n')
 
 
 def welcome_user():
     """Acquaintance."""
+    global name
     name = string('May I have your name? ')
-    print('Hello, {0}'.format(name))
+    print('Hello, {0}!'.format(name), end = '\n\n')
+
+def even(num):
+    """Parity check."""
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def question():
+    """Question to the user."""
+    num = randint(1, 100)
+    global correct 
+    correct = 'Correct!'
+    print('Question: {0}'.format(num))
+    answer = string('Your answer: ')
+    even_num = even(num)
+    if even_num and answer == 'yes':
+        return correct
+    elif even_num == False and answer == 'no':
+        return correct
+    elif even_num == False and answer == 'yes':
+        return "'yes' is wrong answer ;(. Correct answer was 'no'."
+    elif even_num and answer == 'no':
+        return "'no' is wrong answer ;(. Correct answer was 'yes'."
+    else:
+        pass
+
+
+def game():
+    """Game."""
+    rounds = 3
+    for i in range(rounds):
+        result = question()
+        if result == correct:
+            print(correct)
+        else:
+            if result != None:
+                print(result)
+            print('Let\'s try again, {0}!'.format(name))
+            return
+
+
+    print('Congratulation, {0}!'.format(name))
+    
--- brain_games/scripts/brain-even.py
+++ brain_games/scripts/brain-even.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-"""Script."""
+"""Script game brain-even."""
 
 from brain_games import cli
 
@@ -8,7 +8,9 @@ from brain_games import cli
 def main():
     """Run script."""
     cli.welcome()
+    cli.rules_of_game_even()
     cli.welcome_user()
+    cli.game()
 
 
 if __name__ == '__main__':
