4fd0c4a30f3f317820add292b3e910f313af9c04
turantul <python_p@ukr.net>
2020-06-28 14:18:51 +0300

    add game prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+"""Brain prime game."""
+
+from random import randint
+
+RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def round():
+    num = randint(0, 100)
+    if is_prime(num):
+        answer = 'yes'
+    else:
+        answer = 'no'
+    return num, answer
+
+
+def is_prime(num):
+    pick = 2
+    while pick * pick <= num and num % pick != 0:
+        pick += 1
+    return pick * pick > num
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,15 @@
+# -*- codinf:utf-8 -*-
+
+"""Script game brain-prime."""
+
+from brain_games.engine import start
+from brain_games.games import prime
+
+
+def main():
+    """Run brain-prime game."""
+    start(prime)
+
+
+if __name__ == '__main__':
+    main()
